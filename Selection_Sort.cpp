a6d763e3843e90941fc35af78be4badcf415ff0f
stark0dh <dhanajaymourya531@gmail.com>
2022-08-28 09:47:05 +0530

    Error Sorted !!

--- Selection_Sort.cpp
+++ Selection_Sort.cpp
@@ -4,7 +4,7 @@ int selection_sort(int arr[],int size){
     for (int i = 0; i < (size-1); i++)
     {
         int minIndex = i;
-        for (int j= i+1 ; i < size; i++)
+        for (int j= i+1 ; j < size; j++)
         {
             if(arr[j]<arr[minIndex]){
                 minIndex = j ;
